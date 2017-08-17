de9359d822453bcceb808ea0f6a74896bcc80c83
Alex Loyko <alex.loyko96@gmail.com>
2017-08-17 11:36:53 -0400

    Intersections importer 0.0.1

---
+++ test/test.js
@@ -0,0 +1,19 @@
+/**
+ * @file The main entry point for the OpenAddresses importer's unit-tests.
+ */
+
+'use strict';
+
+/*require( './schema' );
+
+require( './import');
+require( './importPipeline');
+require( './parameters' );
+require( './streams/cleanupStream' );
+require( './streams/deduplicatorStream');
+require( './streams/documentStream' );
+require( './streams/germanicAbbreviationStream');
+require( './streams/isUSorCAHouseNumberZero' );
+require( './streams/recordStream' );
+*/
+require( './isValidCsvRecord' );
